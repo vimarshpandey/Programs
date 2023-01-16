ec8a81199cfc8c2f10b19b12532b81c8cc010360
vimpandey <vimpandey2001@gmail.com>
2023-01-16 21:53:39 +0530

    DSA Files Edit

--- DSA/Array/Del-in-Array.c
+++ DSA/Array/Del-in-Array.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<stdio.h>
 int main()
 {
@@ -36,43 +35,4 @@ int main()
     {
         printf("arr[%d]=%d\n",i,arr[i]);
     }
-=======
-#include<stdio.h>
-int main()
-{
-    int arr[100],size,index;
-    printf("Enter the size of the array: \n");
-    scanf("%d",&size);
-    printf("Enter the elements of the array: \n");
-    for(int i=0;i<size;i++)
-    {
-        scanf("%d",&arr[i]);
-    }
-    printf("Array elements before deletion: \n");
-    for(int i=0;i<size;i++)
-    {
-        printf("arr[%d]=%d\n",i,arr[i]);
-    }
-    printf("Enter the index of the element which you want to deleat: \n");
-    scanf("%d",&index);
-    if(index>size)
-    {
-        printf("Deletion is not possible.\n");
-    }
-    else
-    {
-        int i=index+1;
-        while(i<size)
-        {
-            arr[i-1]=arr[i];
-            i=i+1;
-        }
-        size=size-1;
-    }
-    printf("Array elements after deletion: \n");
-    for(int i=0;i<size;i++)
-    {
-        printf("arr[%d]=%d\n",i,arr[i]);
-    }
->>>>>>> ad42450dbfa4a1d3e79cfb630e87a46151ba50eb
 }
