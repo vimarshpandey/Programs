d16c5f5444f9df18e4abab32a5d5dc1bb1dade86
vimpandey <vimpandey2001@gmail.com>
2023-01-16 12:01:47 +0530

    error correction

--- DSA/Searching and Sorting/Bubble-Sort.c
+++ DSA/Searching and Sorting/Bubble-Sort.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<stdio.h>
 int main()
 {
@@ -33,40 +32,4 @@ int main()
     {
         printf("arr[%d]=%d\n",i,arr[i]);
     }
-=======
-#include<stdio.h>
-int main()
-{
-    int arr[100],size,i,j,temp;
-    printf("Enter the size of array\n");
-    scanf("%d",&size);
-    printf("Enter the elements of array\n");
-    for(i=0;i<size;i++)
-    {
-        scanf("%d",&arr[i]);
-    }
-    printf("Array before bubble sort\n");
-    for(i=0;i<size;i++)
-    {
-        printf("arr[%d]=%d\n",i,arr[i]);
-    }
-    
-    for(i=0;i<(size-1);i++)
-    {
-        for(j=0;j<(size-i-1);j++)
-        {
-            if(arr[j]>arr[j+1])
-            {
-                temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
-            }
-        }
-    }
-    printf("Array after bubble sort\n");
-    for(i=0;i<size;i++)
-    {
-        printf("arr[%d]=%d\n",i,arr[i]);
-    }
->>>>>>> ad42450dbfa4a1d3e79cfb630e87a46151ba50eb
 }
