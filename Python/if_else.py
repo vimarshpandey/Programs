164a2953f868f4f305cf9946a185b5017a1c5cbb
vimarshpandey <vimarsh240501@gmail.com>
2023-08-03 10:49:46 +0530

    changes in py file

---
+++ Python/if_else.py
@@ -0,0 +1,4 @@
+import sys
+x=int(input("Enter a number"))
+type(x)
+sys.getsizeof(x)
---
+++ Python/if_else_condition.py
@@ -0,0 +1,17 @@
+x = 10
+y = 20
+
+print(x)
+print(type(y)) #for printing the type of variables
+
+if x > y:
+    print("Hey!")
+elif x == y:
+    print("Whats's up")
+elif x < y:
+    print("That's cool")
+else:
+    print(":)")
+
+for i in range(1, 11):
+    print(i * 2)
